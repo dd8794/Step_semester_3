b3e1be1993124c32bb0b84ed76c8570a8091dc9d
Deekshitha <drapanenideekshitha@gmail.com>
2026-09-19 23:34:38 +0530

    week 6 oops

---
+++ src/main/java/week6/class_problems/EmployeeCompanyInformation.java
@@ -0,0 +1,47 @@
+package week6.class_problems;
+
+public class EmployeeCompanyInformation {
+
+    static class Employee {
+
+        String empName;
+        double salary;
+
+        static String companyName =
+                "Bright Horizon Technologies";
+
+        static int employeeCount = 0;
+
+        Employee(String empName, double salary) {
+
+            this.empName = empName;
+            this.salary = salary;
+
+            employeeCount++;
+        }
+
+        static void printCompanyInfo() {
+
+            System.out.println(companyName);
+
+            System.out.println(
+                    "Employees on record: "
+                            + employeeCount
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee employee1 =
+                new Employee("Aarav", 50000);
+
+        Employee employee2 =
+                new Employee("Riya", 60000);
+
+        Employee employee3 =
+                new Employee("Karan", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}
